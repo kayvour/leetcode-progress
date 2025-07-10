32bd229b2364655b1e0e0e616eff04e43b68fd6c
Kaivalya Vaidya <109648033+kayvour@users.noreply.github.com>
2025-07-10 10:16:39 +0530

    Time: 10 ms (88.57%), Space: 14.1 MB (35.11%) - LeetHub

---
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,11 @@
+import re
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        return s == s[::-1]
+        
